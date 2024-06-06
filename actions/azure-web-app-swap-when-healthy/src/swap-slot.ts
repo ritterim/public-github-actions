6847b9cb48530f1b96b57030c6b22c56cc96227f
Andrew Rady <andrew.rady@ritterim.com>
2024-06-06 13:55:54 -0600

    filter for app from management client

--- actions/azure-web-app-swap-when-healthy/src/swap-slot.ts
+++ actions/azure-web-app-swap-when-healthy/src/swap-slot.ts
@@ -1,5 +1,4 @@
 import { WebSiteManagementClient } from '@azure/arm-appservice';
-import { ResourceManagementClient } from '@azure/arm-resources';
 import { DefaultAzureCredential } from '@azure/identity';
 
 export async function SwapApps(
@@ -8,22 +7,20 @@ export async function SwapApps(
   slot: string): Promise<void> {
     const credential = new DefaultAzureCredential();
     const managementClient = new WebSiteManagementClient(credential, subscriptionId);
-    const resourceClient = new ResourceManagementClient(credential, subscriptionId);
-    let resourceGroups = [];
-    for await (const item of resourceClient.resourceGroups.list()) {
-      resourceGroups.push(item);
+    let webApps = [];
+    for await (const item of managementClient.webApps.list()) {
+      webApps.push(item);
     }
     
-    // TODO: The webAppName is the App Service name and needs to be more generic to get the proper resource group
-    const app = resourceGroups.find(x => x.name == webAppName);
+    const app = webApps.find(x => x.name == webAppName);
 
     if (app == null) {
       return;
     }
 
     return await managementClient.webApps.beginSwapSlotAndWait(
-        /* resourceGroupName: */ `${app?.name}`,
-        /* appName: */ `${webAppName}`,
+        /* resourceGroupName: */ `${app.resourceGroup}`,
+        /* appName: */ `${app.name}`,
         /* slot */ 'staging',
         /* slotSwapEntity */ { preserveVnet: false, targetSlot: slot });
 }
